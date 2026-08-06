eb4d79a058c4518a66476ba312f8e9effebcccfa
dishar37 <134776959+dishar37@users.noreply.github.com>
2026-08-06 19:25:38 +0530

    Create 01_data_cleaning.sql

---
+++ SQL/01_data_cleaning.sql
@@ -0,0 +1,38 @@
+/*
+====================================================
+Superstore Sales Analysis
+File: 01_data_cleaning.sql
+Purpose:
+Basic data quality checks before analysis.
+====================================================
+*/
+
+-- Total records
+SELECT COUNT(*) AS total_orders
+FROM orders;
+
+-- Check for duplicate Row IDs
+SELECT row_id, COUNT(*)
+FROM orders
+GROUP BY row_id
+HAVING COUNT(*) > 1;
+
+-- Check NULL values
+
+SELECT *
+FROM orders
+WHERE
+order_id IS NULL
+OR customer_id IS NULL
+OR sales IS NULL
+OR profit IS NULL;
+
+-- Count unique customers
+
+SELECT COUNT(DISTINCT customer_id) AS unique_customers
+FROM orders;
+
+-- Count unique products
+
+SELECT COUNT(DISTINCT product_id) AS unique_products
+FROM orders;
